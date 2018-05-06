c93b82e6985e2d4d6d66246752584126f3166ccd
maximilianchang <maximilianachang@gmail.com>
2018-05-05 22:08:54 -0700

    parse url for q_id

--- public/app/js/question-view.js
+++ public/app/js/question-view.js
@@ -15,7 +15,8 @@
       }
     })
 
-    xmlQuestionDetail.open("GET", remoteHost + 'question_detail', true)
+    var q_id = getWindowQuestionID()
+    xmlQuestionDetail.open("GET", remoteHost + 'question_detail?q_id=' + q_id, true)
     xmlQuestionDetail.send(null)
   }
 
@@ -28,4 +29,26 @@
   };
 
   window.QuestionView = QuestionView;
+
+  function getWindowQuestionID() { 
+    var url_string = window.location.href
+    var url = new URL(url_string);
+    var q_id = url.searchParams.get("q_id");
+    return q_id
+  }
+
+  function GetURLParameter(sParam)
+  {
+      var sPageURL = window.location.search.substring(1);
+      var sURLVariables = sPageURL.split('&');
+      for (var i = 0; i < sURLVariables.length; i++) 
+      {
+          var sParameterName = sURLVariables[i].split('=');
+          if (sParameterName[0] == sParam) 
+          {
+              return sParameterName[1];
+          }
+      }
+  }​
+
 })();
