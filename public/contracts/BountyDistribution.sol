d6b06607284d9a42bf0b5d8a40a9af2e7d3594ce
Claire Shu <claireshu@DN0a1f4e09.SUNet>
2018-05-05 22:55:10 -0700

    solidity contract v2

--- public/contracts/BountyDistribution.sol
+++ public/contracts/BountyDistribution.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.4.21;
 contract BountyDistribution {
     struct Question {
         uint256 hash;
-        uint256 bounty;
+        uint256 bounty; // denominated in WEI. 1 ether = 10^18 wei
         address askerAddr;
         uint256 answerHash;
+        bool settled;
+        bool isValue; // checks for existence of key
     }
 
     mapping (uint256 => Question) public questions;
@@ -16,14 +18,16 @@ contract BountyDistribution {
     }
 
     function distributeBounty(address _answerer, uint256 _aHash, uint256 _qHash) public {
-        Question storage q = questions[_qHash];
-        _answerer.transfer(q.bounty);
-        q.answerHash = _aHash;
+        require(questions[_qHash].isValue && !questions[_qHash].settled);
+        _answerer.transfer(questions[_qHash].bounty);
+        questions[_qHash].answerHash = _aHash;
+        questions[_qHash].settled = true;
     }
 
     function refundBounty(uint256 _qHash) public {
-        Question storage q = questions[_qHash];
-        q.askerAddr.transfer(q.bounty);
+        require(questions[_qHash].isValue && !questions[_qHash].settled);
+        questions[_qHash].askerAddr.transfer(questions[_qHash].bounty);
+        questions[_qHash].settled = true;
         // should we set answerHash to something?
     }
 
@@ -31,11 +35,18 @@ contract BountyDistribution {
         // can also use msg.sender and msg.value
         // who controls whether bounty == msg.value
         require(msg.value >= _bounty);
-        questions[_qHash] = Question({hash: _qHash, bounty: _bounty, askerAddr: _asker, answerHash: 0}); // there's no null
+        questions[_qHash] = Question({
+            hash: _qHash,
+            bounty: _bounty,
+            askerAddr: _asker,
+            settled: false,
+            isValue: true,
+            answerHash: 0}); // there's no null
     }
 
     // kill function?
     // fallback function?
     // msg.value --> how much ether was spent in making this call
     // error handling with whether or not questions[_qHash] exists
+    // state is required or else refunds can be manipulated
 }
