1f40addcd192ab649c7639235b4c7b3cd74803c0
maximilianchang <maximilianachang@gmail.com>
2018-05-26 14:46:38 -0700

    offloading checks

--- public/contracts/BountyDistribution.sol
+++ public/contracts/BountyDistribution.sol
@@ -8,7 +8,6 @@ contract BountyDistribution {
     }
 
     // Congress Variables
-    address[] members; 
     mapping (address => bool) membership; 
     mapping (address => MemberProposal) addProposals; 
     mapping (address => MemberProposal) removeProposals; 
@@ -36,20 +35,20 @@ contract BountyDistribution {
 
         if (addAddress) { 
             addProposals[recipient] = MemberProposal({
-                recipient: recipient, 
-                minExecutionDate: now + debatingPeriodInMinutes * 1 minutes, 
-                numberOfVotes: 0, 
+                recipient: recipient,
+                minExecutionDate: now + debatingPeriodInMinutes * 1 minutes,
+                numberOfVotes: 0,
                 currentResult: 0,
-                open: true, 
+                open: true,
                 isValid: true
             });
         } else {
             removeProposals[recipient] = MemberProposal({
-                recipient: recipient, 
-                minExecutionDate: now + debatingPeriodInMinutes * 1 minutes, 
-                numberOfVotes: 0, 
+                recipient: recipient,
+                minExecutionDate: now + debatingPeriodInMinutes * 1 minutes,
+                numberOfVotes: 0,
                 currentResult: 0,
-                open: true, 
+                open: true,
                 isValid: true
             });
         }
@@ -100,6 +99,10 @@ contract BountyDistribution {
         uint256 bounty; // denominated in WEI. 1 ether = 10^18 wei
         address askerAddr;
         mapping(uint256 => Answer) answers;
+        mapping(uint256 => uint256) indexes; 
+        uint minExecutionDate; 
+
+        uint numAnswers; 
         bool settled;
         bool isValue; // checks for existence of key
     }
@@ -116,42 +119,64 @@ contract BountyDistribution {
     mapping (uint256 => Question) public questions;
     address contractCreator;
 
-    function BountyDistribution() public { // constructor
+    constructor() public { // constructor
         contractCreator = msg.sender;
+        membership[msg.sender] = true; 
     }
 
     function addAnswer(uint256 _qHash, uint256 _aHash) public {
         require(questions[_qHash].isValue && !questions[_qHash].settled);
-        questions[_qHash].answers[_aHash] = Answer({
-            answerAddr: msg.sender, 
-            answerHash: _aHash, 
+        questions[_qHash].answers[questions[_qHash].numAnswers] = Answer({
+            answerAddr: msg.sender,
+            answerHash: _aHash,
             numUpvotes: 0,
             isValue: true
         }); 
+        questions[_qHash].indexes[_aHash] = questions[_qHash].numAnswers; 
+        questions[_qHash].numAnswers++; 
     }
 
     function addUpvote(uint256 _qHash, uint256 _aHash) public { 
         require(questions[_qHash].isValue && !questions[_qHash].settled);
-        require(questions[_qHash].answers[_aHash].isValue && !questions[_qHash].answers[_aHash].voted[msg.sender]);
 
-        questions[_qHash].answers[_aHash].numUpvotes++; 
-        questions[_qHash].answers[_aHash].voted[msg.sender] = true; 
+        uint256 idx = questions[_qHash].indexes[_aHash]; 
+        require(questions[_qHash].answers[idx].isValue && !questions[_qHash].answers[idx].voted[msg.sender]);
+
+        questions[_qHash].answers[idx].numUpvotes++; 
+        questions[_qHash].answers[idx].voted[msg.sender] = true; 
     }
 
-    function distributeBounty(address _answerer, uint256 _aHash, uint256 _qHash) public {
+    function distributeBounty(uint256 _qHash) public {
         require(questions[_qHash].isValue && !questions[_qHash].settled);
-        _answerer.transfer(questions[_qHash].bounty);
-        questions[_qHash].settled = true;
+        require(now > questions[_qHash].minExecutionDate); 
+
+
+        if (questions[_qHash].numAnswers == 0) {
+            refundBounty(_qHash); 
+        } else {
+            address highestAddress = questions[_qHash].answers[0].answerAddr; 
+            uint numUpvotes = questions[_qHash].answers[0].numUpvotes;
+
+            for(uint i = 1; i < questions[_qHash].numAnswers; i++)
+            {
+                if (questions[_qHash].answers[i].numUpvotes > numUpvotes) { 
+                    highestAddress = questions[_qHash].answers[i].answerAddr; 
+                    numUpvotes = questions[_qHash].answers[i].numUpvotes; 
+                }
+            }  
+            highestAddress.transfer(questions[_qHash].bounty);
+            questions[_qHash].settled = true;
+        }
     }
 
-    function refundBounty(uint256 _qHash) public {
+    function refundBounty(uint256 _qHash) private {
         require(questions[_qHash].isValue && !questions[_qHash].settled);
         questions[_qHash].askerAddr.transfer(questions[_qHash].bounty);
         questions[_qHash].settled = true;
         // should we set answerHash to something?
     }
 
-    function collectBounty(address _asker, uint256 _qHash, uint256 _bounty) public payable {
+    function collectBounty(address _asker, uint256 _qHash, uint256 _bounty, uint minExecutionDate) public payable {
         // can also use msg.sender and msg.value
         // who controls whether bounty == msg.value
         require(msg.value >= _bounty);
@@ -160,7 +185,9 @@ contract BountyDistribution {
             bounty: _bounty,
             askerAddr: _asker,
             settled: false,
-            isValue: true
+            isValue: true,
+            numAnswers: 0,
+            minExecutionDate: minExecutionDate
         }); // there's no null
     }
 
